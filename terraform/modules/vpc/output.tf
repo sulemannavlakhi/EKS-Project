17a4883964866a57a138e23a43295aab59d05848
Your Name <you@example.com>
2026-03-27 04:10:52 -0400

    outputting public and private subnets

---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,15 @@
+output "private_subnet_1" {
+  value = aws_subnet.private_subnet_1.id
+}
+
+output "private_subnet_2" {
+  value = aws_subnet.private_subnet_2.id
+}
+
+output "public_subnet_1" {
+  value = aws_subnet.public_subnet_1.id
+}
+
+output "public_subnet_2" {
+  value = aws_subnet.public_subnet_2.id
+}
