a5146d0b6afc70d08bd2594fd60953f846c191f4
Your Name <you@example.com>
2026-03-27 05:01:05 -0400

    variablized subnets

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,19 @@
+variable "subnet_private1_id" {
+  type        = string
+  description = "private1 subnet id from the vpc module"
+}
+
+variable "subnet_private2_id" {
+  type        = string
+  description = "private2 subnet id from the vpc module"
+}
+
+variable "subnet_public1_id" {
+  type        = string
+  description = "public 1 subnet id from the vpc module"
+}
+
+variable "subnet_public2_id" {
+  type        = string
+  description = "public 2 subnet id from the vpc module"
+}
