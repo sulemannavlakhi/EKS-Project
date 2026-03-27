8b1fee2fbe335d3c6f0e9941b3e74195bf5769d4
Your Name <you@example.com>
2026-03-27 05:00:26 -0400

    fixing syntax errors with outputs

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -1,15 +1,15 @@
-output "private_subnet_1" {
-  value = aws_subnet.private_subnet_1.id
+output "public_subnet1_id" {
+  value = aws_subnet.public_subnet1.id
 }
 
-output "private_subnet_2" {
-  value = aws_subnet.private_subnet_2.id
+output "public_subnet2_id" {
+  value = aws_subnet.public_subnet2.id
 }
 
-output "public_subnet_1" {
-  value = aws_subnet.public_subnet_1.id
+output "private_subnet1_id" {
+  value = aws_subnet.private_subnet1.id
 }
 
-output "public_subnet_2" {
-  value = aws_subnet.public_subnet_2.id
+output "private_subnet2_id" {
+  value = aws_subnet.private_subnet2.id
 }
